24741d472ffcb1916c58d22690d8401724ed5b85
Neeraj Shrivas <shrivasneeraj01@gmail.com>
2024-10-06 19:22:17 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
 //your JS code here. If required.
+let divs = document.querySelectorAll(".square");
+
+divs.forEach(t => {
+    console.log(t);
+    t.addEventListener("mouseover", () => {
+        for (let i = 0; i < divs.length; i++) {
+            if (t == divs[i]) {
+                divs[i].style.backgroundColor = "#E6E6FA"
+                // continue;
+            } else {
+                divs[i].style.backgroundColor = "#6F4E37"
+            }
+        }
+    })
+    t.addEventListener("mouseout", () => {
+        divs.forEach(el => {
+            el.style.backgroundColor = "#E6E6FA"
+        });
+    })
+});
